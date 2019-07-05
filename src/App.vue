694d026f8ccb04368535f2413142abe617b38492
tani <tani@1-10.com>
2019-07-05 16:58:49 +0900

    [update]ロード処理実装

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
-    test
+    <p>ロード進捗率：{{ progressRate }}</p>
+    <p v-if="loadState">ロードが完了しました。</p>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 
 export default {
-  name: 'app',
-  components: {
+  computed: {
+    ...mapState({
+      progressRate: state => state.progressRate,
+      loadState: state => state.loadState
+    }),
+  },
+  created() {
+    this.$store.dispatch('initLoading');
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -3,14 +3,93 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let d = 0
+const procArr = [] // 処理結果の格納配列
+
 export default new Vuex.Store({
   state: {
-
+    progressRate: 0,
+    loadState: false
   },
   mutations: {
+    /**
+     * addProgressRate 進捗率
+     */
+    addProgressRate(state, num) {
+      state.progressRate = num
+    },
 
+    /**
+     * loadComplete ロード完了状態
+     */
+    loadComplete(state) {
+      state.loadState = true
+    },
   },
   actions: {
+    /**
+     * initLoading 初期読み込み時
+     * @param commit
+     * @returns {Promise<*>}
+     */
+    async initLoading({ commit }) {
+      const promsArr = [
+        // Promiseを返り値にもつ非同期処理を記載する
+        sampleProc(1000),
+        sampleProc(3000),
+        sampleProc(5000),
+        sampleProc(2000),
+        sampleProc(6500),
+        sampleProc(1500)
+      ]
 
+      allProgress(promsArr, commit, () => {
+        commit('loadComplete')
+      })
+    }
   }
 })
+
+/**
+ * allProgress 進捗率管理
+ * @param proms
+ * @param progressCallBack
+ * @param completeCallBack
+ * @returns {Promise<[any, any, any, any, any, any, any, any, any, any] | never>}
+ */
+const allProgress = (proms, commit, completeCallBack) => {
+  proms.forEach((p) => {
+    p.then((res) => {
+      calcProgress(res, proms, commit)
+    })
+  })
+  return Promise.all(proms).then(() => {
+    completeCallBack()
+  })
+}
+
+/**
+ * calcProgress 進捗率計算
+ * @param res
+ * @param promsArr
+ * @param callBack
+ */
+const calcProgress = (res, promsArr, commit) => {
+  d++
+  procArr.push(res)
+  commit('addProgressRate', d * 100 / (promsArr.length))
+}
+
+
+/***************************** サンプル処理ここから *****************************/
+
+const sampleProc = (ms) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log(`waited ${ms}`)
+      resolve()
+    }, ms)
+  })
+}
+
+/***************************** サンプル処理ここまで *****************************/
