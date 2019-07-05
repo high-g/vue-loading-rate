8122c0251a96d88e59dcb4754527d741e73297f9
tani <tani@1-10.com>
2019-07-05 18:43:11 +0900

    [update]ロード完了アニメーション

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <p>ロード進捗率：{{ progressRate }}</p>
-    <p v-if="loadState">ロードが完了しました。</p>
+    <div class="load" v-if="!finish">
+      <p >ロード進捗率：{{ progressRate }}%</p>
+      <div class="rate">
+        <div class="rate--bar" :style="calcRate"></div>
+      </div>
+    </div>
+    <p v-if="finish">ロードが完了しました。</p>
   </div>
 </template>
 
@@ -9,11 +14,28 @@
 import { mapState } from 'vuex'
 
 export default {
+  data() {
+    return {
+      finish: false
+    }
+  },
   computed: {
     ...mapState({
       progressRate: state => state.progressRate,
       loadState: state => state.loadState
     }),
+    calcRate() {
+      return `width: ${this.progressRate}%`
+    }
+  },
+  watch: {
+    loadState(res) {
+      if(res) {
+        setTimeout(() => {
+          this.finish = true
+        }, 1000)
+      }
+    }
   },
   created() {
     this.$store.dispatch('initLoading');
@@ -21,7 +43,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,5 +51,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: column;
+}
+
+.rate {
+  width: 100%;
+  border-radius: 5px;
+  overflow: hidden;
+}
+
+.rate--bar {
+  height: 10px;
+  background: #000;
+  transition: width .5s ease-in-out;
 }
 </style>
